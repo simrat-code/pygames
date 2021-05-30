f80fb9b4ad92b4db41c9a381ef74ca206f630dfb
simrat singh <simrat.khokhar@gmail.com>
2021-05-30 15:19:49 +0530

    fix bug

--- monopoly/runme.py
+++ monopoly/runme.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     sys.stdout.write(sys.version)
-    if sys.version_info[0] != "3":
+    if sys.version_info[0] != 3:
         sys.stdout.write("\nkindly use python 3.x to run the program\n")
         sys.exit(1)
 
