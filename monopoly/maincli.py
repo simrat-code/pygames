69eba90eefc6a005ad49d0ae54d110f687d373de
simrat singh <simrat.khokhar@gmail.com>
2021-05-31 00:19:13 +0530

    display text modified

--- monopoly/maincli.py
+++ monopoly/maincli.py
@@ -7,17 +7,16 @@ from utils import colorText
 
 def printPosition(participants):
     if not isinstance(participants, tuple): raise TypeError("must pass tuple")
-    print(" "*8, end="")
+    # print(" "*8, end="")
     for obj in participants:
         if not isinstance(obj, player.Player): continue
         print(
-            " " +
             colorText(
                 f"{obj.getPosition():02} {obj.getIncome():>5}", obj.name
             )
-            , end=""
+            , end="  "
         )
-    print("")
+    # print("")
 
 def main():
     myboard = board.Board('config.xml')
@@ -29,23 +28,23 @@ def main():
     blue = player.Player("Blue")
     green = player.Player("Green")
     yellow = player.Player("Yellow")
-    participants = (red, green, blue)
+    participants = (red, green, blue, yellow)
     turn=0
     try:
         while True:
-            # var = input(f"{turn} enter to continue: ")
             turn += 1
             gameover = 0
             for token in participants:
                 if not token.isActive(): continue
                 gameover += 1
+                print(
+                    highlighter(
+                        f"[{token.getName():6}] "
+                    )
+                    , end=""
+                )
                 printPosition(participants)
-                vartext = f"[{token.getName():6}] {token.getPosition():02} | " + \
-                    f"trip {token.getTrip()} round {turn}  " + \
-                    f"press enter: "
-                # print(f">{vartext}<")
-                vartext = highlighter(vartext)
-                vartext = input(f"{vartext}")
+                vartext = input("press enter: ")
                 processturn.play(token, myboard)
             # IF only one player is left, declare it winner
             if gameover <= 1: break
@@ -61,3 +60,6 @@ def main():
                 break
     except KeyboardInterrupt as e:
         print('\ncaught user interrupt Ctrl+C, exiting...')
+    finally:
+        print(f"total turns: {turn}\n")
+
